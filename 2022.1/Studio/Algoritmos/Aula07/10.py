2667ae20329d401abed04de5af5a3e4e4d9733c9
carlosChanpio70 <carlosalexterra@gmail.com>
2022-05-02 20:06:06 -0300

    10.py done
    How could I forget this one

--- 2022.1/Studio/Algoritmos/Aula07/10.py
+++ 2022.1/Studio/Algoritmos/Aula07/10.py
@@ -1,6 +1,18 @@
-def main(x,y):
-    return x + y
+def main():
+    n_of_n = int(input("Quantos números deseja inserir? "))
+    n_input(n_of_n)
 
-x = int(input())
-y = int(input())
-print(main(x,y))
+def n_input(n_of_n):
+    loop = 1
+    while loop <= n_of_n:
+        n = int(input(f"Digite o número {loop}: "))
+        loop += 1
+        print(f"{n}! = {fatorial(n)}")
+
+def fatorial(x):
+    nr = 1
+    for i in range(1,x+1):
+        nr *= i
+    return nr
+
+main()
