b818dde90dca57d2e88f1d73c2f6cb33ed75c51f
carlosChanpio70 <carlosalexterra@gmail.com>
2022-05-04 19:02:01 -0300

    18.py done

--- 2022.1/Studio/Algoritmos/Aula07/18.py
+++ 2022.1/Studio/Algoritmos/Aula07/18.py
@@ -4,7 +4,7 @@ def main():
 
 def perfect_n_finder_amount(x):
     loop,_ = 1,0
-    while _<=x:
+    while _<x:
         n = perfect_n_finder(loop)
         loop += 1
         if n:
@@ -13,16 +13,12 @@ def perfect_n_finder_amount(x):
 
 def perfect_n_finder(x):
     str,loop,n,sum = f"{x} = 1",0,0,1
-    while loop<=x/2:
-        loop += 1
-        if loop!=1 and x%loop==0:
-            str += f"+{loop}"
-            sum += loop
-        else:
-            n += 1
-    if loop==sum:
-        return str
-    else:
-        return False
+    for loop in range(2,x+1):
+        if sum<=x and x%loop==0:
+            str+=f"+{loop}"
+            sum+=loop
+        if x==sum:
+            return str
+    return False
 
 main()
