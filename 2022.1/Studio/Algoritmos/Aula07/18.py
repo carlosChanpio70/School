31caf40a5bfbc5bc0f47503d13d108084994b26c
carlosChanpio70 <carlosalexterra@gmail.com>
2022-05-05 21:40:06 -0300

    Finally fixed the damn 18.py

--- 2022.1/Studio/Algoritmos/Aula07/18.py
+++ 2022.1/Studio/Algoritmos/Aula07/18.py
@@ -1,9 +1,12 @@
+from asyncore import loop
+
+
 def main():
     n_of_n = int(input("Quantos números perfeitos deseja ver? "))
     perfect_n_finder_amount(n_of_n)
 
 def perfect_n_finder_amount(x):
-    loop,find_counter = 1,0
+    loop,find_counter = 2,0
     while find_counter<x:
         n = perfect_n_finder(loop)
         loop += 1
@@ -12,17 +15,15 @@ def perfect_n_finder_amount(x):
             find_counter+=1
 
 def perfect_n_finder(x):
-    str,sum = f"{x} = 1",1
+    str,sum,loop,loop_check = f"{x} = 1",1,2,int(x/2)
     if x%2==0:
-        for loop in range(2,int(x/2)+1):
+        while loop<=loop_check:
             if x%loop==0:
                 str+=f"+{loop}"
                 sum+=loop
-            if sum==x:
-                return str
-            if sum>x:
-                return False
-    else:
-        return False
+            loop+=1
+    if sum==x:
+        return str
+    return False
 
 main()
