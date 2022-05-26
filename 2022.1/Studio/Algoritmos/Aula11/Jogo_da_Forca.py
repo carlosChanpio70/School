14ef14a5b74bcf5bb3d4d2c204a774372c2cfbfa
carlosChanpio70 <carlosalexterra@gmail.com>
2022-05-25 23:35:25 -0300

    Some upgrades

--- 2022.1/Studio/Algoritmos/Aula11/Jogo_da_Forca.py
+++ 2022.1/Studio/Algoritmos/Aula11/Jogo_da_Forca.py
@@ -3,27 +3,28 @@ def file_path(): return "2022.1\Studio\Algoritmos\Aula11\words.txt"
 
 
 def main():
-    losses = [];wins = [];game_state = False
+    losses = [];played = [];wins = []
+    game_state = False
     word_in = word_get().upper()
-    table_print(word_in, losses, wins)
+    table_print(word_in, losses, wins, played)
     while not game_state:
-        losses, wins = _play(word_in, losses, wins)
+        losses, wins, played = _play(word_in, losses, wins, played)
         game_state = win_check(word_in, losses, wins)
     if game_state == 1:
         print("Você venceu com a tabela:")
     else:
         print("Você perdeu com a tabela:")
-    table_print(word_in, losses, wins)
+    table_print(word_in, losses, wins, played)
 
 
 def word_get():
-    file = open(file_path(),"r")
+    file = open(file_path(), "r")
     words = file.readlines()
     file.close()
-    return words[randint(0,len(words)-1)].strip()
+    return words[randint(0, len(words)-1)].strip()
 
 
-def _play(word_in, losses, wins):
+def _play(word_in, losses, wins, played):
     play = input("Digite uma letra: ").upper()
     if play in losses+wins or len(play) != 1:
         print("Valor já digitado/inválido digite novamente.")
@@ -32,12 +33,14 @@ def _play(word_in, losses, wins):
         wins.append(play)
     else:
         losses.append(play)
-    table_print(word_in, losses, wins)
-    return losses, wins
+    played.append(play)
+    table_print(word_in, losses, wins, played)
+    return losses, wins, played
 
 
 def win_check(word_in, losses, wins):
-    win_n = 0;word_len = len(word_in)
+    win_n = 0
+    word_len = len(word_in)
     for i in word_in:
         if i in losses+wins:
             win_n += 1
@@ -48,14 +51,13 @@ def win_check(word_in, losses, wins):
     return False
 
 
-def table_print(word_in, losses, wins):
-    word_r = ""
+def table_print(word_in, losses, wins, played):
+    word_r = "";played_r = ""
     for i in word_in:
-        if i in wins:
-            word_r += i
-        else:
-            word_r += "_"
+        if i in wins:   word_r += i
+        else:           word_r += "_"
         word_r += " "
+    for i in played:    played_r += f" {i}"
     if len(losses) > 0: head = "0"
     else:               head = " "
     if len(losses) > 1: torso = "|"
@@ -66,13 +68,10 @@ def table_print(word_in, losses, wins):
     else:               leg_right = " "
     if len(losses) > 4: arm_right = "/"
     else:               arm_right = " "
-    if len(losses) > 5:arm_left = "\\"
+    if len(losses) > 5: arm_left = "\\"
     else:               arm_left = " "
-    table = " ____\n |  |\n"
-    table += f" | {arm_left}{head}{arm_right}\n"
-    table += f" |  {torso}\n"
-    table += f" | {leg_left} {leg_right}  "
-    table += word_r
+    table = f"\n ____{played_r}\n |  |\n | {arm_left}{head}{arm_right}\n"
+    table += f" |  {torso}\n | {leg_left} {leg_right}  {word_r}"
     print(table)
 
 
