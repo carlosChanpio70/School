9555352a612811df22c367dab8f2126f83f483b3
carlosChanpio70 <carlosalexterra@gmail.com>
2022-11-09 15:19:58 -0300

    .

--- 2022.2/Studio/Orientação_de_Objetos/Trabalho_Final/main.py
+++ 2022.2/Studio/Orientação_de_Objetos/Trabalho_Final/main.py
@@ -1,9 +1,13 @@
 from model.usuario import Usuario
 
+
+#Shift | Bibli0Tec
+
+
 def main():
     usuario=Usuario()
     usuario.setFilepath("2022.2/Studio/Orientação_de_Objetos/Trabalho_Final/files/usuarios.txt")
-    usuario.writeUsuarios([0,"Ahoy",0,"Ahoy","123"])
+    usuario.readUsuarios(0)
 
 if __name__=="__main__":
     main()
--- 2022.2/Studio/Orientação_de_Objetos/Trabalho_Final/model/usuario.py
+++ 2022.2/Studio/Orientação_de_Objetos/Trabalho_Final/model/usuario.py
@@ -11,5 +11,5 @@ class Usuario():
         usuarios.write(f"{list[0]},{list[1]},{list[2]},{list[3]},{list[4]}\n")
         usuarios.close()
 
-    def readUsuarios(self):
+    def readUsuarios(self,id):
         usuarios=open(self.__filepath,"r")
