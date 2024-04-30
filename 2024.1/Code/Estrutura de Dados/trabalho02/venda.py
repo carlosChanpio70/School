fb030f09f03eec65bba587372b29103b2c0644f1
carlosChanpio70 <52218734+carlosChanpio70@users.noreply.github.com>
2024-04-30 16:53:27 -0300

    Progress

--- 2024.1/Code/Estrutura de Dados/trabalho02/venda.py
+++ 2024.1/Code/Estrutura de Dados/trabalho02/venda.py
@@ -1,51 +1,118 @@
-import os
-import struct
+import os,struct
 
 def binary_to_venda(binary):
-    return (binary[0], binary[1], binary[2].decode('latin-1'))
+    return [struct.unpack('i',binary[:4])[0],struct.unpack('f', binary[4:8])[0], binary.decode('latin-1')[8:].strip()]
 
-def gravar_venda(venda):
+def venda_to_binary(venda):
+    return (struct.pack('i', venda[0]) + struct.pack('f', venda[1]) + bytes(venda[2].ljust(10), 'latin-1'))
+
+def check_if_exists(venda):
     f = open(os.path.join(os.path.dirname(__file__), 'vendas.bin'), 'rb')
     j = 0
-    for i in range(f.read()/18):
+    for i in range(len(f.read())//18):
         f.seek(i*18)
         b = f.read(18)
-        if binary_to_venda(b) == None:
+        venda2 = binary_to_venda(b)
+        print(venda2)
+        print(venda)
+        if venda2 == None:
             j = i
             break
-        elif binary_to_venda(b)[0] == venda[0]:
+        elif venda2[0] == venda[0]:
             j = None
             break
-        elif binary_to_venda(b)[2] > venda[2]:
+        elif venda2[2][:2] > venda[2][:2]:
             j = None
             break
     f.close()
+    return j
+
+def adicionar_venda(venda):
+    j = check_if_exists(venda)
     if j is None:
-        print('ja existe')
-        return
-    f = open(os.path.join(os.path.dirname(__file__), 'vendas.bin'), 'ab')
-    f.seek(j*18)
-    f.write(struct.pack('i', venda[0]))
-    f.write(struct.pack('f', venda[1]))
-    f.write(bytes(venda[2].ljust(10), 'latin-1'))
+        print('Ja existe')
+    else:
+        f = open(os.path.join(os.path.dirname(__file__), 'vendas.bin'), 'ab')
+        f.seek(j*18)
+        f.write(venda_to_binary(venda))
+        f.close()
+        print('Gravado')
+
+def ler_venda(vendedor):
+    f = open(os.path.join(os.path.dirname(__file__), 'vendas.bin'), 'rb')
+    j = None
+    for i in range(len(f.read())//18):
+        f.seek(i*18)
+        venda = binary_to_venda(f.read(18))
+        if venda[0] == vendedor:
+            j = i
+            break
     f.close()
-    print('gravado')
+    return j
+
+def excluir_venda(vendedor):
+    j = ler_venda(vendedor)
+    if j is None:
+        print('Nao existe')
+    else:
+        f = open(os.path.join(os.path.dirname(__file__), 'vendas.bin'), 'rb+')
+        f.seek((j+1)*18)
+        rd = f.read()
+        f.seek(j*18)
+        f.write(rd)
+        f.close()
+        print('Excluido')
 
 def main():
-    pass
+    venda = [None, None, None]
+    while True:
+        print('1 - Criar o arquivo de dados')
+        print('2 - Incluir um determinado vendedor no arquivo')
+        print('3 - Excluir um determinado vendedor no arquivo')
+        print('4 - Alterar o valor total da venda de um determinado vendedor de um determinado mês')
+        print('5 - Imprimir os registros na saída padrão')
+        print('6 - Consultar o vendedor com maior valor da venda')
+        print('7 - Finalizar o programa')
+        op = int(input('Escolha uma opção: '))
+        if op == 1:
+            pass
+        elif op == 2:
+            venda[0] = int(input('Codigo do vendedor: '))
+            venda[1] = float(input('Valor da venda: '))
+            venda[2] = input('Mes/Ano: ')
+            adicionar_venda(venda)
+        elif op == 3:
+            excluir_venda(int(input('Codigo do vendedor: ')))
+            pass
+        elif op == 4:
+            pass
+        elif op == 5:
+            pass
+        elif op == 6:
+            pass
+        elif op == 7:
+            break
 
+main()
 
-f = open(os.path.join(os.path.dirname(__file__), 'vendas.bin'), 'wb+')
-f.write(struct.pack('i', 23))
-f.write(struct.pack('f', 8.5))
-f.write(bytes('mm/aaaa'.ljust(10), 'latin-1'))
-f.seek(0)
-b = struct.unpack('i', f.read(4))[0]
-print(b)
-f.seek(4)
-b = struct.unpack('f', f.read(4))[0]
-print(b)
-f.seek(8)
-b = f.read(10).decode('latin-1')
-print(b)
-f.close()
+#* Fazer um programa denominado de venda.py que manipule um arquivo binário contendo registros descritos pelos seguintes campos:
+
+#*   codigo_vendedor, valor_da_venda e mes_ano
+
+#* Codigo_vendedor: integer
+#* Valor_venda : float
+#* mes_ano : string da forma (mm/aaaa)
+#* Entrada de Dados
+
+#* O programa vendas.py deverá apresentar uma tela com as opções disponíveis e executar cada opção.
+#* A manipulação do arquivo em questão é feita através da execução das operações disponibilizadas pelo seguinte menu:
+
+#* 1 Criar o arquivo de dados;
+#* 2 Incluir um determinado vendedor no arquivo;
+#* 3 Excluir um determinado vendedor no arquivo;
+#* 4 Alterar o valor total da venda de um determinado vendedor de um determinado mês;
+#* 5 Imprimir os registros na saída padrão;
+#* 6 Consultar o vendedor com maior valor da venda
+#* 7 Finalizar o programa.
+
+#* Não deve existir mais de um registro no arquivo com mesmos valores nos campos código_vendedor e mes.
